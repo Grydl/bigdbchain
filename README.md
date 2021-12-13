0985cb0d6ef2c4897684110b026a6d7664c798bf
Charles Azanlekor <c.azanlekor@grydl.com>
2021-12-13 08:25:18 +0000

    installation bigchaindb

--- README.md
+++ README.md
@@ -1,2 +1,209 @@
-# bigdbchain
-Projet Java Bigdbchain
+# Bigchaindb
+Projet Java BigchainDB
+
+## Créer un réseau BigchainDB
+
+### Installation VM
+
+Créer une VM avec Ubuntu 18.04
+
+Se connecter à cette VM en ssh et noter les adresses IP externes de chaque nœud.
+
+Ouvrir les ports  : **26656**  et  **9984**
+
+``ssh -i PATH_TO_PRIVATE_KEY USERNAME@EXTERNAL_IP``
+
+### Installation Docker
+
+Installer les packages afin d’utiliser apt à accéder au repository  via HTTPS
+``sudo apt-get install apt-transport-https ca-certificates curl software-properties-common``
+
+Ajouter la clé GPG officiel de Docker
+``curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -``
+
+Ajouter le repository à APT sources
+``sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"``
+
+Mettre à jour ensuite la base de données APT
+``sudo apt-get update``
+
+Installer Docker Community Edition(CE)
+``sudo apt-get -y install docker-ce``
+
+Ajouter docker au programmes de démarrage du Serveur
+``sudo systemctl status docker``
+
+Vérifier ensuite si Docker est bien installer sur l’application
+``sudo docker run hello-world``
+
+Pour utiliser la commande docker sans devoir ajouter sudo
+- ``sudo usermod -aG docker ${USER}``
+- ``Installer pip``
+- ``sudo apt-get -y install python-pip``
+- ``sudo pip install pip --upgrade``
+
+Installer docker compose
+``sudo pip install docker-compose``
+
+Tester si l’installation est ok
+``docker-compose --version``
+
+## Lancer Bigchaindb Docker
+
+Exécuter la commande ci-dessous pour démmarrer BigchainDB :
+
+``docker pull grydl/bigchaindb:all-in-one``
+
+```
+docker run \
+--detach \
+--name grydl_bigchaindb \
+--publish 9984:9984 \
+--publish 9985:9985 \
+--publish 27017:27017 \
+--publish 26657:26657 \
+--volume $HOME/bigchaindb_docker/mongodb/data/db:/data/db \
+--volume $HOME/bigchaindb_docker/mongodb/data/configdb:/data/configdb \
+--volume $HOME/bigchaindb_docker/tendermint:/tendermint \
+grydl/bigchaindb:all-in-one
+```
+
+Avec docker-compose , demarrer Bigchaindb avec la commande ci-dessous, 
+ne pas oublier de se placer dans le repertoire ./docker avant 
+
+``docker-compose -up &``
+
+Vérifier que vous le container docker est bien démmarer
+
+```docker ps```
+
+Connecter à l’adresse ci dessous et vérifier que cela fonctionne  
+http://localhost:9984/
+ou
+http://votre_ip_publique:9984/
+
+**Stopper ensuite le container**
+
+``docker ps``
+
+Récupérer le container_id
+
+``docker stop container_id``
+
+Stopper le container avec la commande ci-dessous dans le cas d'utilisationde  docker-compose
+docker.
+ne pas oublier de se placer dans le repertoire ./docker avant
+
+``docker-compose down ``
+
+## Configurer le réseau Bigchainbb
+
+Maintenant nous pouvons faire la configuration du réseau
+
+Chaque bigchaindb est identifier par :
+- **hostname :** il s’agit du nom de domaine or de son IP
+pub_key.value : la clé publique du node Tendermint, la clé privée se trouve dans le repertoire .
+*.tendermint/config/priv_validator.json*
+
+- **node_id :** l’identifiant du noeud Tendermint, pour obtenir le node_id , il faut lancer la commande
+
+``tendermint show_node_id``
+
+
+Il faut maintenant créer le fichier **genesis**  qui se trouve dans le répertoire  :
+*tendermint/config/genesis.json*
+
+Une fois le fichier genesis rempli , il faut le partager avec tous les membres, les membres doivent le mettre dans le repertoire :
+/.tendermint/config
+
+Tous les membres partagent le même **chain_id** , **genesis time**, et la liste des validators
+
+Il faut ensuite modifier le fichier : *tendermint/config/config.toml*
+
+```
+moniker = "Name of our node"
+create_empty_blocks = false
+log_level = "main:info,state:info,*:error"
+
+persistent_peers = "<Member 1 node id>@<Member 1 hostname>:26656,\
+<Member 2 node id>@<Member 2 hostname>:26656,\
+<Member N node id>@<Member N hostname>:26656,"
+
+send_rate = 102400000
+recv_rate = 102400000
+
+recheck = false
+```
+
+
+
+# Intallation de BigchainDB
+
+Il est aussi possible d'installer BigChainDB sur son poste local plutôt que d'utiliser lîmage Docker.
+En suivant le guide ci-dessous
+
+Mettre à jour la machine
+
+```
+sudo apt update
+sudo apt full-upgrade
+```
+
+Installer python
+
+``
+sudo apt install -y python3-pip libssl-dev
+``
+
+Mise à jour de pip a une version ulterieur à 19 car Bigdbchain à besoin de gevent
+
+``
+sudo pip3 install -U pip
+``
+
+Installer la derniere version de Bigchaindb
+
+``
+sudo pip3 install bigchaindb==2.0.0
+``
+
+Vérifier que bigchaindb est bien installée
+
+``
+bigchaindb --version
+``
+
+Configurer
+
+``
+bigchaindb configure
+``
+
+A la question API Server bind? (default `localhost:9984`).
+Repondre :  **0.0.0.0:9984**
+
+### Installation MongoDB
+
+Lancer la commande ci-dessous pour installer mongodb
+
+``
+sudo apt install mongodb
+``
+
+### Installer Tendermint
+
+Lancer la commande ci-dessous
+```
+sudo apt install -y unzip
+wget https://github.com/tendermint/tendermint/releases/download/v0.31.5/tendermint_v0.31.5_linux_amd64.zip
+unzip tendermint_v0.31.5_linux_amd64.zip
+rm tendermint_v0.31.5_linux_amd64.zip
+sudo mv tendermint /usr/local/bin
+```
+
+Configurer Tendermint
+Lancer la commande
+
+```
+tendermint init
+```
--- src/main/java/com/grydl/orange/bigchaindb/BigchainDBJavaDriverUsageExample.java
+++ src/main/java/com/grydl/orange/bigchaindb/BigchainDBJavaDriverUsageExample.java
@@ -8,10 +8,8 @@ import java.util.TreeMap;
 import com.bigchaindb.builders.BigchainDbConfigBuilder;
 import com.bigchaindb.builders.BigchainDbTransactionBuilder;
 import com.bigchaindb.constants.Operations;
-import com.bigchaindb.model.FulFill;
-import com.bigchaindb.model.GenericCallback;
-import com.bigchaindb.model.MetaData;
-import com.bigchaindb.model.Transaction;
+import com.bigchaindb.cryptoconditions.Fulfillment;
+import com.bigchaindb.model.*;
 import com.bigchaindb.util.Base58;
 
 import net.i2p.crypto.eddsa.EdDSAPrivateKey;
@@ -33,7 +31,7 @@ public class BigchainDBJavaDriverUsageExample {
      * @throws IOException
      * @throws InterruptedException
      */
-    public static void main(String args[]) throws Exception {
+    public static <assetId> void main(String args[]) throws Exception {
 
         BigchainDBJavaDriverUsageExample examples = new BigchainDBJavaDriverUsageExample();
 
@@ -50,8 +48,14 @@ public class BigchainDBJavaDriverUsageExample {
         KeyPair keys3 = BigchainDBJavaDriverUsageExample.getKeys();
 
 
-        System.out.println(Base58.encode(keys.getPublic().getEncoded()));
-        System.out.println(Base58.encode(keys.getPrivate().getEncoded()));
+        System.out.println("User 1 : "+Base58.encode(keys.getPublic().getEncoded()));
+        System.out.println("User 1 : "+Base58.encode(keys.getPrivate().getEncoded()));
+
+        System.out.println("User 2 : "+Base58.encode(keys2.getPublic().getEncoded()));
+        System.out.println("User 2 : "+Base58.encode(keys2.getPrivate().getEncoded()));
+
+        System.out.println("User 3 : "+Base58.encode(keys3.getPublic().getEncoded()));
+        System.out.println("User 3 : "+Base58.encode(keys3.getPrivate().getEncoded()));
 
         // create New asset
         Map<String, String> assetData = new TreeMap<String, String>() {{
@@ -64,22 +68,36 @@ public class BigchainDBJavaDriverUsageExample {
         // create metadata
         MetaData metaData = new MetaData();
         metaData.setMetaData("reference", "Emission monnaie");
-        metaData.setMetaData("total", "100");
+        metaData.setMetaData("total", "145");
         System.out.println("(*) Metadata Prepared..");
 
         //execute CREATE transaction
         String txId = examples.doCreate(assetData, metaData, keys);
 
-        //create transfer metadata
-        MetaData transferMetadata = new MetaData();
-        transferMetadata.setMetaData("Transsfer ", "amount");
-        System.out.println("(*) Transfer Metadata Prepared..");
+        String assetId = txId;
+
 
         //let the transaction commit in block
         Thread.sleep(5000);
 
+        System.out.println("######################################## id = "+ assetId+ " TX "+ txId);
+
+        //create transfer metadata
+        MetaData transferMetadata = new MetaData();
+        transferMetadata.setMetaData("Transfer ", "amount");
+        System.out.println("(*) Transfer Metadata Prepared..");
+
         //execute TRANSFER transaction on the CREATED asset
-        examples.doTransfer(txId, transferMetadata, keys,keys2);
+        String txId2 = examples.doTransfer(assetId,txId, transferMetadata, keys,keys2);
+
+
+        Thread.sleep(5000);
+        System.out.println("######################################## id = "+ assetId+ " TX "+ txId2);
+
+        //execute TRANSFER transaction on the CREATED asset
+        //String  txId3 = examples.doTransferInput(assetId, txId2, transferMetadata, keys,keys2,keys3);
+
+        //System.out.println("######################################## id = "+ assetId+ " TX "+ txId3);
 
     }
 
@@ -100,7 +118,9 @@ public class BigchainDBJavaDriverUsageExample {
             @Override
             public void transactionMalformed(Response response) {
                 System.out.println("malformed " + response.message());
+                System.out.println("malformed " + response.request().toString());
                 onFailure();
+
             }
 
             @Override
@@ -112,6 +132,7 @@ public class BigchainDBJavaDriverUsageExample {
             @Override
             public void otherError(Response response) {
                 System.out.println("otherError" + response.message());
+                System.out.println("malformed " + response.request().toString());
                 onFailure();
             }
         };
@@ -163,7 +184,6 @@ public class BigchainDBJavaDriverUsageExample {
                     .addOutput(String.valueOf(145), (EdDSAPublicKey) keys.getPublic())
                     .operation(Operations.CREATE)
                     .buildAndSign((EdDSAPublicKey) keys.getPublic(), (EdDSAPrivateKey) keys.getPrivate())
-
                     .sendTransaction(handleServerResponse());
 
             System.out.println("(*) CREATE Transaction sent.. - " + transaction.getId());
@@ -184,10 +204,10 @@ public class BigchainDBJavaDriverUsageExample {
      * @param keysSender keys to sign and verify transactions
      * @param keysReceiver keys to receive the output
      */
-    public void doTransfer(String txId, MetaData metaData, KeyPair keysSender, KeyPair keysReceiver) throws Exception {
+    public String doTransfer(String assetId, String txId, MetaData metaData, KeyPair keysSender, KeyPair keysReceiver) throws Exception {
 
         Map<String, String> assetData = new TreeMap<String, String>();
-        assetData.put("id", txId);
+        assetData.put("id", assetId);
 
         try {
 
@@ -196,14 +216,14 @@ public class BigchainDBJavaDriverUsageExample {
             fulfill.setOutputIndex(0);
             fulfill.setTransactionId(txId);
 
-
             //build and send TRANSFER transaction
             Transaction transaction = BigchainDbTransactionBuilder
                     .init()
                     .addInput(null, fulfill, (EdDSAPublicKey) keysSender.getPublic())
                     .addOutput(String.valueOf(140), (EdDSAPublicKey) keysReceiver.getPublic())
-                    .addOutput(String.valueOf(5), (EdDSAPublicKey) keysSender.getPublic())
-                    .addAssets(txId, String.class)
+                    .addOutput(String.valueOf(3), (EdDSAPublicKey) keysReceiver.getPublic())
+                    .addOutput(String.valueOf(2), (EdDSAPublicKey) keysSender.getPublic())
+                    .addAssets(assetId, String.class)
                     .addMetaData(metaData)
                     .operation(Operations.TRANSFER)
                     .buildAndSign((EdDSAPublicKey) keysSender.getPublic(), (EdDSAPrivateKey) keysSender.getPrivate())
@@ -211,11 +231,66 @@ public class BigchainDBJavaDriverUsageExample {
 
             System.out.println("(*) TRANSFER Transaction sent.. - " + transaction.getId());
 
+            return transaction.getId();
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
+        return null;
+
+    }
+
+
+
+    public String doTransferInput(String assetID, String txId, MetaData metaData, KeyPair keysSender, KeyPair keysReceiver, KeyPair other) throws Exception {
+
+        Map<String, String> assetData = new TreeMap<String, String>();
+        assetData.put("id", assetID);
+
+        try {
+
+            //which transaction you want to fulfill?
+            FulFill fulfill = new FulFill();
+            fulfill.setOutputIndex(0);
+            fulfill.setTransactionId(txId);
+
+            //which transaction you want to fulfill?
+            FulFill fulfill2 = new FulFill();
+            fulfill2.setOutputIndex(1);
+            fulfill2.setTransactionId(txId);
+
+            //which transaction you want to fulfill?
+            FulFill fulfill3 = new FulFill();
+            fulfill3.setOutputIndex(2);
+            fulfill3.setTransactionId(txId);
+
+            //build and send TRANSFER transaction
+            Transaction transaction = BigchainDbTransactionBuilder
+                    .init()
+                    .addInput((String) null, fulfill, (EdDSAPublicKey) keysReceiver.getPublic())
+                    .addInput((String) null, fulfill2, (EdDSAPublicKey) keysReceiver.getPublic())
+                    //.addInput((String)null, fulfill2, (EdDSAPublicKey) keysSender.getPublic())
+                    //.addInput((String)null, fulfill3, (EdDSAPublicKey) keysSender.getPublic())
+                    //.addOutput(String.valueOf(5), (EdDSAPublicKey) other.getPublic())
+                    .addOutput(String.valueOf(140), (EdDSAPublicKey) other.getPublic())
+                    .addOutput(String.valueOf(5), (EdDSAPublicKey) other.getPublic())
+                    .addAssets(assetID, String.class)
+                    .addMetaData(metaData)
+                    .operation(Operations.TRANSFER)
+                    .buildAndSign((EdDSAPublicKey) keysReceiver.getPublic(), (EdDSAPrivateKey) keysReceiver.getPrivate())
+                    .sendTransaction(handleServerResponse());
+
+            System.out.println("(*) TRANSFER Transaction sent.. - " + transaction.getId());
+
+            return transaction.getId();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
